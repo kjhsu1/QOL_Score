9e0c7c9ec1f53f5cb922ff6e05f1f10f3c90f512
username <kenta.sfjp@gmail.com>
2025-02-06 08:23:10 -0800

    up

--- run_this_daily.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Set path
-cd ~/Code/Personal/QOL_Score/
-
-# Change sys argv to appropriate entry number
-python3 QOL_input_extraction.py 150 | python3 QOL_score_compute.py
--- run_this_daily_all_in_one.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Set path
-cd ~/Code/Personal/QOL_Score/
-
-# Change sys argv to appropriate entry number
-python3 All_in_one_QOL_input_extraction.py 1 | python3 All_in_one_QOL_score_compute.py
