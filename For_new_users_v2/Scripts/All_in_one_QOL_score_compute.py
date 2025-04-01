7e30e5e3fc1b43256d27cda16a704508b3589779
username <kenta.sfjp@gmail.com>
2025-03-31 22:35:37 -0700

    up

--- For_new_users_v2/Scripts/All_in_one_QOL_score_compute.py
+++ For_new_users_v2/Scripts/All_in_one_QOL_score_compute.py
@@ -8,8 +8,67 @@ import tkinter as tk
 from tkinter import messagebox
 from PIL import Image, ImageTk
 import random
+import requests
 
+username = os.getenv('USER')
 first_part_of_path = f"/Users/{username}/Downloads/QOL_Score/For_new_users_v2"
+
+# dict with all user info
+# includes "User Analysis Requests" database
+all_users = {
+    "Kenta": {
+        "NOTION_TOKEN": "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",
+        "DATABASE_ID": "1924d9b143a980719cabc4f151bc30fb"
+    },
+
+    "Kazuma":{
+        "NOTION_TOKEN": "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",
+        "DATABASE_ID": "1954d9b143a981019212fbe32c21a6a1" 
+    },
+
+    "User_Analysis_Requests_Database":{
+        "NOTION_TOKEN": "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",
+        "DATABASE_ID": "1ad4d9b143a980e7806ece9c6a0eb626"
+    }
+
+}
+
+# CHANGE THIS FOR OTHER USERS
+user = "Kenta"
+DATABASE_ID = all_users[user]["DATABASE_ID"]
+NOTION_TOKEN = all_users[user]["NOTION_TOKEN"]
+# Analysis Request Database
+AR_DATABASE_ID = all_users["User_Analysis_Requests_Database"]["DATABASE_ID"]
+AR_NOTION_TOKEN = all_users["User_Analysis_Requests_Database"]["NOTION_TOKEN"]
+
+headers = {
+    "Authorization": "Bearer " + NOTION_TOKEN,
+    "Content-Type": "application/json",
+    "Notion-Version": "2022-06-28",
+}
+
+# header for Analysis Requests Database
+ar_headers = {
+    "Authorization": "Bearer " + AR_NOTION_TOKEN,
+    "Content-Type": "application/json",
+    "Notion-Version": "2022-06-28",
+}
+
+def fetch_latest_streak():
+    url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
+    response = requests.post(url, headers=headers)
+    data = response.json()
+    results = data.get("results", [])
+    
+    if not results:
+        return None
+    
+    # Extract the entry with the highest numerical "Name" value
+    highest_entry = max(results, key=lambda x: int(x["properties"].get("Name", {}).get("title", [{}])[0].get("plain_text", "0")))
+    
+    # Get the Streak property from the highest entry
+    streak_property = highest_entry.get("properties", {}).get("Streak", {}).get("number", None)
+    return streak_property
 
 def calculate_qol_score(data):
     wake_time = data["Wake Times"][0]
@@ -171,7 +230,7 @@ def display_qol_score(score, good_streak, bad_streak):
     root.mainloop()
 
 # Main function to read JSON data and calculate/display QOL score and streaks
-def main():
+
     input_json = sys.stdin.read()
     
     if not input_json.strip():
@@ -222,6 +281,30 @@ def main():
             json.dump(streaks, file, indent=4)
 
     display_qol_score(score, streaks['good_streak'], streaks['bad_streak'])
+
+def main():
+    input_json = sys.stdin.read()
+    
+    if not input_json.strip():
+        print("No input data provided.")
+        return
+    
+    data = json.loads(input_json)
+
+    score = calculate_qol_score(data)
+
+    # Track streaks
+    latest_streak = fetch_latest_streak()
+    good_streak = 0
+    bad_streak = 0
+    if latest_streak == 0:
+        pass
+    elif latest_streak > 0:
+        good_streak = latest_streak
+    elif latest_streak < 0:
+        bad_streak = abs(latest_streak)    
+    
+    display_qol_score(score, good_streak, bad_streak)
 
 if __name__ == "__main__":
     main()
