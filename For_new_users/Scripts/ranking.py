48ea6f9dbc96f598940df1f507ddbf42f23b47c3
username <kenta.sfjp@gmail.com>
2025-05-14 23:28:41 -0700

    added new user eoin

--- For_new_users/Scripts/ranking.py
+++ For_new_users/Scripts/ranking.py
@@ -26,6 +26,11 @@ all_users = {
         "DATABASE_ID": "1954d9b143a981019212fbe32c21a6a1" 
     },
 
+    "Eoin":{
+        "NOTION_TOKEN": "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",
+        "DATABASE_ID": "1f44d9b143a980559a2bffbe8e7ab9b5" 
+    },
+
     "User_Analysis_Requests_Database":{
         "NOTION_TOKEN": "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",
         "DATABASE_ID": "1ad4d9b143a980e7806ece9c6a0eb626"
